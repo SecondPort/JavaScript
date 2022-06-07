5cb4364a8b7828f89b6b0e4a5d8512d1d4ce986c
SecondPort <gomezmanulm@gmail.com>
2022-06-07 11:37:26 -0300

    buenas practicas

--- Tipos de datos/var-let-const.js
+++ Tipos de datos/var-let-const.js
@@ -4,3 +4,13 @@ console.log(nombre)
 
 const apellido = "perez"
 console.log(apellido)
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto)
+
+let x,y;
+x = y = 10;
+console.log(x,y)
+
+z = x + y;
+console.log(z)
