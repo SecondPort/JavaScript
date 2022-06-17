43b04156ec82bd6d7642729d99fe060dcfc7d571
SecondPort <gomezmanulm@gmail.com>
2022-06-16 23:36:25 -0300

    operadores

--- Temas-JavaScript/Operadores/operadores.js
+++ Temas-JavaScript/Operadores/operadores.js
@@ -2,3 +2,13 @@ let a = 3, b = 5;
 let z = a + b;
 console.log("Resultado: ",+ z);
 
+let c = 2, d = 6;
+
+console.log("Resultado: ", d / c);
+
+//modulo
+console.log("Resultado: ", d % c);
+
+//potencia
+console.log("Resultado: ", Math.pow(c, d));
+console.log("Resultado: ", d ** c);
