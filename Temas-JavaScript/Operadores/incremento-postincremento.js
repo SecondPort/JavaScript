b1b812eab968765f3498bba4e434e90229bb9a3e
SecondPort <gomezmanulm@gmail.com>
2022-06-17 10:21:42 -0300

    incrementar-decrementar

---
+++ Temas-JavaScript/Operadores/incremento-postincremento.js
@@ -0,0 +1,18 @@
+let a = 2, b = 8, c = 9;
+let z = 0;
+
+//preincremento
+console.log(z= ++a);
+
+//postincremento
+console.log(z = b++);
+//la proxima vez que se use b se va a incrementar porque lo tiene pendiente
+console.log(z = b++);
+
+
+//predecremento
+console.log(z = --c);
+
+//postdecremento
+console.log(z = c--);
+console.log(z = c--);
