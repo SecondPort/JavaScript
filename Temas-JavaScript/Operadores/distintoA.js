9be2a892e15d5b9a16dedbc1375ca7722e5f89b3
SecondPort <gomezmanulm@gmail.com>
2022-06-17 10:39:38 -0300

    distinto A

---
+++ Temas-JavaScript/Operadores/distintoA.js
@@ -0,0 +1,7 @@
+let a = 3, b = 2, c = "3";
+
+let z = a != c;//se revisa el valor sin importar el tipo de dato
+console.log(z);
+
+z = a !== c;//revisa si los valores son iguales y tambien los tipos
+console.log(z);
