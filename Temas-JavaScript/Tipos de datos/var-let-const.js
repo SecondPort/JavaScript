9911f6d5139486e1a816ce2e570333235d2007f0
SecondPort <gomezmanulm@gmail.com>
2022-06-16 23:32:00 -0300

    new files

---
+++ Temas-JavaScript/Tipos de datos/var-let-const.js
@@ -0,0 +1,22 @@
+//buena practica
+let nombre = "juan"
+console.log(nombre)
+
+const apellido = "perez"
+console.log(apellido)
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto)
+
+let x,y;
+x = y = 10;
+console.log(x,y);
+
+z = x + y;
+console.log(z);
+
+let nomCompleto = nombre + " " + apellido;
+console.log(nomCompleto);
+
+let _2nombreCompleto = nombre.concat(" ", apellido);
+console.log(_2nombreCompleto);
