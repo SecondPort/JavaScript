d3106f66285f7b28cdc0682541b925b301b59f3e
SecondPort <gomezmanulm@gmail.com>
2022-06-17 10:47:35 -0300

    .

--- Practicas/mayorEdad.js
+++ Practicas/mayorEdad.js
@@ -1,6 +1,7 @@
 //definir si una persona es mayor de edad o no
+const adulto = 20;
 function esMayor(edad){
-    if(edad >= 18){
+    if(edad >= adulto){
         return true;
     }else{
         return false;
