b3b091cde67764302ac95b5fba51e91c7f522897
SecondPort <gomezmanulm@gmail.com>
2022-06-22 10:45:23 -0300

    OR

---
+++ Temas-JavaScript/Operadores/OR.js
@@ -0,0 +1,9 @@
+let vacaciones = true; diaRest = false;
+
+
+    if (vacaciones || diaRest) {
+        console.log("Puede ir de vacaciones");
+    }
+    else {
+        console.log("No puede ir de vacaciones");
+    }
