ee720a1fd195a542272bba0839c78609bce99b97
SecondPort <gomezmanulm@gmail.com>
2022-06-22 11:02:43 -0300

    and update

--- Temas-JavaScript/Operadores/AND.js
+++ Temas-JavaScript/Operadores/AND.js
@@ -2,8 +2,8 @@ let a = 5;
 let valMin = 0;
 let valMax = 10;
 
-if(a>=valMin && a<=valMax){
+if (a >= valMin && a <= valMax) {
     console.log("El valor esta dentro del rango");
-}else{
+} else {
     console.log("El valor esta fuera del rango");
 }
