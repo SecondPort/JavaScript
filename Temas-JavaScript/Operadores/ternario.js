9596c365510836991dede7496cf8d6947b5aeedb
SecondPort <gomezmanulm@gmail.com>
2022-06-22 10:50:03 -0300

    ternario

---
+++ Temas-JavaScript/Operadores/ternario.js
@@ -0,0 +1,8 @@
+let result = (3>2) ? "3 es mayor que 2" : "3 no es mayor que 2";
+console.log(result);
+
+/*------------------------------------------------*/
+
+let num = 9
+let par = (num % 2 == 0) ? "El numero es par" : "El numero es impar";
+console.log(par);
