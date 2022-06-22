d3f81d23f8b172e53e1bea3829462e4bb4991bdc
SecondPort <gomezmanulm@gmail.com>
2022-06-22 11:02:59 -0300

    mas precedencia

--- Temas-JavaScript/Operadores/precedencia-operadores.js
+++ Temas-JavaScript/Operadores/precedencia-operadores.js
@@ -1,8 +1,20 @@
 let a = 3, b = 2, c = 1;
 
-let z = a * b + c;
+let q = a * b + c;
 //se evalua de izquierda a derecha
+console.log(q);
+
+q = c + a * b;
+console.log(q);
+
+/*----------------------------------------*/
+
+let x = 5;
+let y = 10;
+let z = ++x + y--;
+console.log(x);
+console.log(y);
 console.log(z);
 
-z = c + a * b;
-console.log(z);
+let result = 5 + 4 * 6 / 3;
+console.log(result);
