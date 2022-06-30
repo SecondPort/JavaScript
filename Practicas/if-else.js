f35eb0997dea46d40b3c57560c22c294bd814d7f
SecondPort <gomezmanulm@gmail.com>
2022-06-30 11:01:19 -0300

    practica-if/else

---
+++ Practicas/if-else.js
@@ -0,0 +1,14 @@
+let numero = 3;
+
+if (numero == 1) {
+    console.log("numero 1");
+}
+else if (numero == 2) {
+    console.log("numero 2");
+}
+else if (numero == 3) {
+    console.log("numero 3");
+}
+else{
+    console.log("No es 1, 2 o 3");
+}
