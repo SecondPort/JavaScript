a34acaed5d8ec3d6862ef2148882c2cb85486529
SecondPort <gomezmanulm@gmail.com>
2022-07-06 17:45:29 -0300

    switch

---
+++ Temas-JavaScript/Sentencias-de-control/switch.js
@@ -0,0 +1,18 @@
+let numero = 2;
+
+let numText = "valor desconocido";
+
+switch(numero) {
+    case 1:
+        numText = "uno";
+        break;
+    case 2:
+        numText = "dos";
+        break;
+    case 3:
+        numText = "tres";
+        break;
+    default:
+        numText = "valor desconocido";
+}
+console.log(numText);
