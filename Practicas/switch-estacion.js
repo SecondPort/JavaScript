6e9918a5f5c37341872d7ca57c720800a01edd35
SecondPort <gomezmanulm@gmail.com>
2022-07-07 08:37:14 -0300

    edit

--- Practicas/switch-estacion.js
+++ Practicas/switch-estacion.js
@@ -2,24 +2,16 @@ let mes = 12;
 let estacion = "";
 
 switch (mes) {
-    case 1:
-    case 2:
-    case 3:
+    case 1: case 2: case 3:
         estacion = "invierno";
         break;
-    case 4:
-    case 5:
-    case 6:
+    case 4: case 5: case 6:
         estacion = "primavera";
         break;
-    case 7:
-    case 8:
-    case 9:
+    case 7: case 8: case 9:
         estacion = "verano";
         break;
-    case 10:
-    case 11:
-    case 12:
+    case 10: case 11: case 12:
         estacion = "otonio";
         break;
     default:
