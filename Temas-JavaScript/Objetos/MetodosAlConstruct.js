a78c8e536387922b835002662859aaddaa696a3b
SecondPort <gomezmanulm@gmail.com>
2022-07-08 16:05:21 -0300

    metodos en el constructor

---
+++ Temas-JavaScript/Objetos/MetodosAlConstruct.js
@@ -0,0 +1,17 @@
+//funcion constructora de objetos de tipo persona
+function Persona(nombre = "Juan",apellido = "Perez",edad = 20){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+}
+
+let padre = new Persona('Lucas','Moyano',50);
+let hijo = new Persona();
+console.log(padre);
+console.log(hijo);
+
+let madre = new Persona('Maria','Perez',40);
+console.log(madre);
+
+padre.nombre = 'Nada';
+console.log(padre);
