c05a0677b0f28f803033ae6e5a434709dd41c6c1
SecondPort <gomezmanulm@gmail.com>
2022-07-08 15:09:08 -0300

    principio obetos

---
+++ Temas-JavaScript/Objetos/Objects.js
@@ -0,0 +1,15 @@
+//los tipos primitvos no contienen metodos ni propiedades
+//los tipos objetos contienen metodos
+
+//tipo primitivo
+let x = 10;
+console.log(x.length);
+
+let persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 20
+}
+
+console.log(persona.nombre.length);
+console.log(persona.apellido);
