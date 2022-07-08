19c72d018d0558caac33043617d95eb2c89864b4
SecondPort <gomezmanulm@gmail.com>
2022-07-08 01:16:56 -0300

    isArray

---
+++ Temas-JavaScript/Arreglos/isArray.js
@@ -0,0 +1,8 @@
+const autos = ["Ford", "Chevrolete", "Toyota", "Honda"];
+console.log(autos);
+
+//isArray
+console.log(Array.isArray(autos));
+
+//si la instancia es un array
+console.log(autos instanceof Array);
