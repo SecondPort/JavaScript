fc9b456b2704714a94b460066d24a67f0e6c5a33
SecondPort <gomezmanulm@gmail.com>
2022-07-08 01:05:40 -0300

    arrays

---
+++ Temas-JavaScript/Arreglos/arrays.js
@@ -0,0 +1,8 @@
+//forma antigua de crear un array
+let autos = new Array("Chevrolet", "Ford", "Nissan", "Honda", "Toyota");
+console.log(autos);
+
+//forma correcta
+const autos2 = ["Chevrolet", "Ford", "Nissan", "Honda", "Toyota"];
+console.log(autos2);
+
