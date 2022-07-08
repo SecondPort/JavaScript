8cf2a4c7bca6aec3238c01f650a0642d286e81cd
SecondPort <gomezmanulm@gmail.com>
2022-07-08 15:57:30 -0300

    metodo set

---
+++ Temas-JavaScript/Objetos/Set.js
@@ -0,0 +1,23 @@
+const persona = {
+    nombre: 'Lucas',
+    apellido: 'Moyano',
+    edad: 20,
+    idioma : 'es_ar',
+    get lang() {
+        return this.idioma.toUpperCase();
+    },
+    set lang (idioma) {
+        this.idioma = idioma.toUpperCase();
+    },
+    get nombreC () {
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+
+console.log(persona.nombreC);
+console.log(persona.lang);
+
+//-------------------------//
+persona.lang = 'en_us';
+console.log(persona.lang);
+
