51ef4b8125b2ca408aa95312e8247144557f309a
SecondPort <gomezmanulm@gmail.com>
2022-07-08 01:15:18 -0300

    agregar al array

---
+++ Temas-JavaScript/Arreglos/agregar.js
@@ -0,0 +1,10 @@
+const autos = ["Ford", "Chevrolet", "Nissan", "Honda", "Toyota"];
+console.log(autos.length);
+
+//agregar al final del array siempre
+autos[autos.length] = "Audi";
+console.log(autos);
+
+//espacio vacio
+autos[7] = "BMW";
+console.log(autos);
