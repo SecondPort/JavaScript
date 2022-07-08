815042c677338ff7855b644704bbab570debbf59
SecondPort <gomezmanulm@gmail.com>
2022-07-08 01:11:34 -0300

    modificar arrays

---
+++ Temas-JavaScript/Arreglos/modificar.js
@@ -0,0 +1,10 @@
+const autos = ['Ford', 'Chevrolete', 'Toyota', 'Honda'];
+console.log(autos);
+
+//despues de modificar el array
+autos [1] = 'Chevrolet';
+console.log(autos);
+
+//agregar un elemento al array
+autos.push('Nissan');
+console.log(autos);
