a8128297e6819223faf1ef27f0d979cafd8d490b
SecondPort <gomezmanulm@gmail.com>
2022-07-09 09:40:26 -0300

    Herencia

---
+++ Temas-JavaScript/Clases/Herencia.js
@@ -0,0 +1,47 @@
+class Vehiculo {
+    constructor(marca, modelo, color) {
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+    }
+    get marca() {
+        return this._marca;
+    }
+    set marca(marca) {
+        this._marca = marca;
+    }
+    get modelo() {
+        return this._modelo;
+    }
+    set modelo(modelo) {
+        this._modelo = modelo;
+    }
+    set color(color) {
+        this._color = color;
+    }
+
+    mostrar() {
+        return `${this._marca} ${this._modelo} ${this._color}`;
+    }
+}
+
+class Auto extends Vehiculo {
+    constructor(marca, modelo, color, puertas) {
+        super(marca, modelo, color);//llamada al constructor de la clase padre
+        this._puertas = puertas;
+    }
+    get puertas() {
+        return this._puertas;
+    }
+    set puertas(puertas) {
+        this._puertas = puertas;
+    }
+    mostrar() {
+        return `${super.mostrar()} ${this._puertas}`;
+    }
+}
+
+let auto1 = new Auto("Ford", "Mustang", "Rojo", 5);
+console.log(auto1.mostrar());
+let nombre = auto1._marca;
+console.log(nombre);
