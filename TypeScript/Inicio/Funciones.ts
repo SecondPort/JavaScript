e4e8d658ab505c4f84a04194ab97bdcd688f3627
SecondPort <gomezmanulm@gmail.com>
2022-07-12 07:30:35 -0300

    updates

--- TypeScript/Inicio/Funciones.ts
+++ TypeScript/Inicio/Funciones.ts
@@ -1,4 +1,18 @@
+//variables
+let saludo = "Hola mundo";
+let numero:number;
+numero = 10;
+
+let x:any;
+x = 10;
+x = "Hola mundo";
+
+const PI = 3.1416;
+
 function saludar(){
-    console.log("Hola mundo");
+    console.log(saludo);
+    console.log(numero);
+    console.log(x);
+    console.log(PI);
 }
 saludar();
--- TypeScript/Variables/Variables.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let saludo = "Hola";
-let numero = 10;
-function saludar() {
-    console.log(saludo);
-}
