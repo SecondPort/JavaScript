68de4cb22174a13c42861954058743e3a9fb5896
SecondPort <gomezmanulm@gmail.com>
2022-07-12 07:49:28 -0300

    tipos genericos

---
+++ TypeScript/Generics/generics.js
@@ -0,0 +1,5 @@
+"use strict";
+let arregloNumeros;
+arregloNumeros = [1, 2, 3, 4, 5];
+//si queremos insertar un string en el arreglo no se puede debido a que el tipo de dato es number
+console.log(arregloNumeros);
---
+++ TypeScript/Generics/generics.ts
@@ -0,0 +1,4 @@
+let arregloNumeros: Array<number>;
+arregloNumeros = [1, 2, 3, 4, 5];
+//si queremos insertar un string en el arreglo no se puede debido a que el tipo de dato es number
+console.log(arregloNumeros);
