dbea5387f1e3e907ce96e557f1340c4bca84def8
SecondPort <gomezmanulm@gmail.com>
2022-07-12 06:28:45 -0300

    funciones para listar y agregar personas

---
+++ Aplicaciones/Listado-de-Persona/js/app.js
@@ -0,0 +1,22 @@
+const personas = [
+    new Persona('Juan','Perez'),
+    new Persona('Pedro','Perez')
+];
+
+function mostrarPersonas(){
+    console.log("ejecutando");
+    let texto = "";
+    for(let i = 0; i < personas.length; i++){
+        texto += `<li> ${personas[i].nombre} ${personas[i].apellido} </li>`;
+        console.log(texto);
+    }
+    document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona(){
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    personas.push(new Persona(nombre,apellido));
+    mostrarPersonas();
+    document.getElementById("forma").reset();
+}
